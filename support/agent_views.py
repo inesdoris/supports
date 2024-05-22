d15893d5261b887efada77cb66364dbfb9f40064
HOUNGBETODE <moberenge@gmail.com>
2024-05-22 17:16:46 +0200

    Full code

---
+++ support/agent_views.py
@@ -0,0 +1,219 @@
+from django.shortcuts import render, redirect
+from .models import *
+
+def index(request):
+    user_id = request.session.get('user_id', None)
+    error = request.session.pop('error', None)
+    success = request.session.pop('success', None)
+    if not user_id:
+        return redirect('/login')
+    user = Utilisateur.objects.get(id=user_id)
+    demandes = Demande.objects.filter(etat__libelle="Envoyée").filter(agent=user)
+    nombre_nouvelles_notifications = Notifications.objects.filter(receiver=user).filter(is_read=False).count()
+    return render(request, 'agent/demandes_affectees.html', {
+        "user": user,
+        "error": error,
+        "success": success,
+        "demandes": demandes,
+        "nombre_nouvelles_notifications": nombre_nouvelles_notifications
+    })
+
+def pending(request):
+    user_id = request.session.get('user_id', None)
+    error = request.session.pop('error', None)
+    success = request.session.pop('success', None)
+    if not user_id:
+        return redirect('/login')
+    user = Utilisateur.objects.get(id=user_id)
+    demandes = Demande.objects.filter(etat__libelle="En cours").filter(agent=user)
+    nombre_nouvelles_notifications = Notifications.objects.filter(receiver=user).filter(is_read=False).count()
+    return render(request, 'agent/demandes_en_cours.html', {
+        "user": user,
+        "error": error,
+        "success": success,
+        "demandes": demandes,
+        "nombre_nouvelles_notifications": nombre_nouvelles_notifications
+    })
+
+def solved(request):
+    user_id = request.session.get('user_id', None)
+    error = request.session.pop('error', None)
+    success = request.session.pop('success', None)
+    if not user_id:
+        return redirect('/login')
+    user = Utilisateur.objects.get(id=user_id)
+    nombre_nouvelles_notifications = Notifications.objects.filter(receiver=user).filter(is_read=False).count()
+    demandes = Demande.objects.filter(etat__libelle="Terminée").filter(agent=user)
+    return render(request, 'agent/demandes_traitees.html', {
+        "user": user,
+        "error": error,
+        "success": success,
+        "demandes": demandes,
+        "nombre_nouvelles_notifications": nombre_nouvelles_notifications
+    })
+
+def admin(request):
+    user_id = request.session.get('user_id', None)
+    error = request.session.pop('error', None)
+    success = request.session.pop('success', None)
+    if not user_id:
+        return redirect('/login')
+    user = Utilisateur.objects.get(id=user_id)
+    traitements = Traiter.objects.filter(utilisateur=user).filter(demande__etat__libelle="Approuvée")
+    nombre_nouvelles_notifications = Notifications.objects.filter(receiver=user).filter(is_read=False).count()
+    return render(request, 'agent/demandes_admin.html', {
+        "user": user,
+        "error": error,
+        "success": success,
+        "traitements": traitements,
+        "nombre_nouvelles_notifications": nombre_nouvelles_notifications
+    })
+
+def mettre_en_traitement(request, id_demande):
+    user_id = request.session.get('user_id', None)
+    if not user_id:
+        return redirect('/login')
+    user = Utilisateur.objects.get(id=user_id)
+    try:
+        demande = Demande.objects.get(id=id_demande)
+        if demande.agent != user:
+            raise("error")
+        if demande.service.categorie :
+            demande.etat = EtatDemande.objects.get(libelle="En cours")
+            demande.save()
+            request.session["success"] = f"La demande n°{demande.id} a été mise en cours de traitement"
+            return redirect("/agent/pending")
+        else:
+            request.session["error"] = f"La mise en traitement d'une demande est tributaire à sa catégorisation. Veuillez donc d'abord catégoriser la demande n°{demande.id}"
+    except:
+        request.session["error"] = f"Echec de mise en traitement de la demande n°{demande.id}"
+    return redirect("/agent")
+
+def abolir_traitement(request, id_demande):
+    user_id = request.session.get('user_id', None)
+    if not user_id:
+        return redirect('/login')
+    user = Utilisateur.objects.get(id=user_id)
+    try:
+        demande = Demande.objects.get(id=id_demande)
+        if demande.agent != user:
+            raise("error")
+        traitement = Traiter.objects.get(demande=demande) if Traiter.objects.filter(demande=demande) else None
+        if traitement and traitement.solution :
+            demande.etat = EtatDemande.objects.get(libelle="Terminée")
+            demande.save()
+            request.session["success"] = f"La demande n°{demande.id} a été mise en fin de traitement"
+            return redirect("/agent/solved")
+        else:
+            request.session["error"] = f"La mise en fin traitement d'une demande est tributaire à son traitement. Veuillez donc renseigner la solution proposée à la demande n°{demande.id}"
+    except:
+        request.session["error"] = f"Echec de mise en fin traitement de la demande n°{demande.id}"
+    return redirect("/agent/pending")
+
+def traiter_demande(request, id_demande):
+    user_id = request.session.get('user_id', None)
+    if not user_id:
+        return redirect('/login')
+    user = Utilisateur.objects.get(id=user_id)
+    services = Service.objects.exclude(libelle="...")
+    nombre_nouvelles_notifications = Notifications.objects.filter(receiver=user).filter(is_read=False).count()
+    demande = Demande.objects.get(id=id_demande)
+    solution_precedemment_renseignee = Traiter.objects.get(demande=demande).solution if Traiter.objects.filter(demande=demande) else None
+    if demande.agent==user:
+        if request.method == "POST":
+            solution = request.POST.get("solution_detaillee")
+            if solution and id_demande:
+                try:
+                    # enregistrement de la solution
+                    traitement = Traiter.objects.get_or_create(demande=demande, utilisateur=user)[0]
+                    traitement.solution = solution
+                    traitement.save()
+                    request.session["success"] = f"La solution à la demande {id_demande} a été appliquée avec succès."
+                except:
+                    request.session["error"] = f"La solution à la demande {id_demande} n'a pas pu être appliquée."
+                return redirect("/agent/pending")
+    return render(request, "agent/traiter_demande.html", {
+        "user": user,
+        "services": services,
+        "description_demande": demande.description,
+        "solution_existante": solution_precedemment_renseignee,
+        "nombre_nouvelles_notifications": nombre_nouvelles_notifications
+    })
+
+def consulter_demande(request, id_demande):
+    user_id = request.session.get('user_id', None)
+    if not user_id:
+        return redirect('/login')
+    user = Utilisateur.objects.get(id=user_id)
+    nombre_nouvelles_notifications = Notifications.objects.filter(receiver=user).filter(is_read=False).count()
+    traitement = None
+    if id_demande:
+        d = Demande.objects.get(id=id_demande)
+        if d.agent != user:
+            raise("error")
+        try:
+            traitement = Traiter.objects.get(demande=d)
+        except:
+            return redirect("/agent/solved")
+    return render(request, "agent/consulter_demande.html", {
+        "user": user,
+        "solution_demande": traitement.solution,
+        "description_demande": traitement.demande.description,
+        "nombre_nouvelles_notifications": nombre_nouvelles_notifications
+    })
+
+def categoriser_demande(request, id_demande):
+    user_id = request.session.get('user_id', None)
+    if not user_id:
+        return redirect('/login')
+    user = Utilisateur.objects.get(id=user_id)
+    categories = CategorieService.objects.all()
+    nombre_nouvelles_notifications = Notifications.objects.filter(receiver=user).filter(is_read=False).count()
+    try:
+        demande = Demande.objects.get(id=id_demande)
+        if demande.agent != user:
+            raise("error")
+        if request.method == "POST":
+            categorie = CategorieService.objects.get(libelle=request.POST.get("categorie_choisie"))
+            # on modifie le service de la demande
+            service = demande.service
+            service.categorie = categorie
+            service.save()
+            # et on assigne le nouveau service à la demande
+            demande.service = service
+            demande.save()
+            request.session["success"] = f"La catégorie '{categorie.libelle}' a été assignée à la demande n°{id_demande}"
+            request.session["error"] = ""
+            return redirect("/agent")
+    except:
+        request.session["error"] = f"La catégorisation de la demande n°{id_demande} a échouée. Veuillez réessayer."
+        request.session["success"] = ""
+        return redirect("/agent")
+    return render(request, "agent/categoriser_demande.html", {
+        "user": user,
+        "categories": categories,
+        "description_demande": demande.description if demande else None,
+        "nombre_nouvelles_notifications": nombre_nouvelles_notifications
+    })
+
+def notifier_admin(request, id_demande):
+    user_id = request.session.get('user_id', None)
+    if not user_id:
+        return redirect('/login')
+    user = Utilisateur.objects.get(id=user_id)
+    nombre_nouvelles_notifications = Notifications.objects.filter(receiver=user).filter(is_read=False).count()
+    demande = Demande.objects.get(id=id_demande)
+    try:
+        if demande.agent != user:
+            raise("error")
+        demande.etat = EtatDemande.objects.get_or_create(libelle="Approuvée")[0]
+        demande.save()
+        Notifications.objects.create(receiver=Utilisateur.objects.filter(profil__id=1).first(), message=f"Une proposition de solution à l'une des demandes de {"M." if demande.demandeur.sexe else "Mme"} {demande.demandeur.nom} {demande.demandeur.prenom} vous a été envoyée par l'agent {user.nom} {user.prenom}.")
+        request.session["success"] = "La solution a été envoyée avec succès."
+        return redirect("/agent/admin")
+    except:
+        request.session["error"] = "L'envoi de la solution a échoué..."
+    return render(request, "agent/demandes_admin.html", {
+        "user": user,
+        "nombre_nouvelles_notifications": nombre_nouvelles_notifications
+    })
--- support/urls.py
+++ support/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from . import agent_views
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -16,6 +17,35 @@ urlpatterns = [
     path('agence/liste', views.liste_agence, name='liste_agence'),
     path('agence/<int:id>/modifier', views.modifier_agence, name='modifier_agence'),
     path('agence/<int:id>/supprimer', views.supprimer_agence, name='supprimer_agence'),
-    
+
     path('chef_agence/formulaire', views.formulaire, name='formulaire'),
+    path('chef_agence/dashboard', views.chef_agence_dashboard, name='dashboard'),
+    path('chef_agence/demandes_en_attente', views.chef_agence_demandes_en_attente, name='demandes_en_attente'),
+    path('chef_agence/demandes_resolues', views.chef_agence_demandes_resolues, name='demandes_resolues'),
+    path('demande/<int:id>/consulter', views.consulter_demande, name='consulter_demande'),
+    path('chef_agence/<int:demande_id>/modifier_demande', views.modifier_demande, name='modifier_demande'),
+    path('chef_agence/<int:id>/supprimer_demande', views.supprimer_demande, name='supprimer_demande'),
+
+    path('notifications', views.notifications, name='notifications'),
+    path('notifications/<int:id>/supprimer', views.supprimer_notification, name='supprimer_notification'),
+
+    # views for agent
+    path('agent/', agent_views.index, name='agent-homepage'),
+    path('agent/pending', agent_views.pending, name='demandes-en-cours-de-traitement'),
+    path('agent/solved', agent_views.solved, name='demandes-resolues'),
+    path('agent/<int:id_demande>', agent_views.mettre_en_traitement, name='mettre-une-demande-en-cours-de-traitement'),
+    path('agent/<int:id_demande>/treat', agent_views.abolir_traitement, name='mettre-une-demande-en-fin-de-traitement'),
+    path('agent/<int:id_demande>/categorize', agent_views.categoriser_demande, name='categoriser-une-demande'),
+    path('agent/<int:id_demande>/categorize', agent_views.categoriser_demande, name='categoriser-une-demande'),
+    path('agent/pending/<int:id_demande>', agent_views.traiter_demande, name='traiter-une-demande'),
+    path('agent/solved/<int:id_demande>', agent_views.consulter_demande, name='consulter-une-demande'),
+    path('agent/admin/', agent_views.admin, name='messages-envoyees-a-l-admin'),
+    path('agent/admin/<int:id_demande>', agent_views.notifier_admin, name='envoi-admin'),
+    
+    # views for admin
+    path('demande/recues', views.liste_demandes_recues, name='liste_demandes_recues'),
+    path('demande/affectees', views.liste_demandes_affectees, name='liste_demandes_affectees'),
+    path('demande/traitees', views.liste_demandes_traitees, name='liste_demandes_traitees'),
+    path('demande/<int:demande_id>/affecter_agent', views.affecter_agent, name='affecter_agent'),
+    path('demande/<int:demande_id>/envoyer_solution', views.envoyer_solution, name='envoyer_solution'),
 ]
