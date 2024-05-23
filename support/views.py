516d05c700d11a8a7712bce7a59c6e019ec7c5dc
Gafouruser <yekiniabdougafour@gmail.com>
2024-05-23 16:21:57 +0100

    Modification de la date demande

--- support/views.py
+++ support/views.py
@@ -394,7 +394,7 @@ def liste_demandes_recues(request):
     if not user_id:
         return redirect('/login')
     user = Utilisateur.objects.get(id=user_id)
-    demandes_recues = Demande.objects.filter(etat=EtatDemande.objects.get(libelle="Envoyée")).order_by('-date_traitement')
+    demandes_recues = Demande.objects.filter(etat=EtatDemande.objects.get(libelle="Envoyée")).order_by('-date_formulation')
     nombre_nouvelles_notifications = Notifications.objects.filter(receiver=user).filter(is_read=False).count()
     return render(request, "demande/recues.html", {
         "error": error,
@@ -411,7 +411,7 @@ def liste_demandes_affectees(request):
     if not user_id:
         return redirect('/login')
     user = Utilisateur.objects.get(id=user_id)
-    demandes_affectees = Demande.objects.filter(etat=EtatDemande.objects.get(libelle="En cours")).order_by('-date_traitement')
+    demandes_affectees = Demande.objects.filter(etat=EtatDemande.objects.get(libelle="En cours")).order_by('-date_formulation')
     nombre_nouvelles_notifications = Notifications.objects.filter(receiver=user).filter(is_read=False).count()
     return render(request, "demande/affectees.html", {
         "error": error,
@@ -429,7 +429,7 @@ def liste_demandes_traitees(request):
         return redirect('/login')
     
     user = Utilisateur.objects.get(id=user_id)
-    demandes_traitees = Demande.objects.filter(etat=EtatDemande.objects.get(libelle="Approuvée")).order_by('-date_traitement')
+    demandes_traitees = Demande.objects.filter(etat=EtatDemande.objects.get(libelle="Approuvée")).order_by('-date_formulation')
     nombre_nouvelles_notifications = Notifications.objects.filter(receiver=user).filter(is_read=False).count()
     
     return render(request, "demande/traitees.html", {
